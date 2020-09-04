897e6a27d9bbcb02b0bb6125a79f87a0657ff0f4
Leonardo Colman Lopes <leonardo.dev@colman.com.br>
2020-09-04 02:01:13 -0300

    Add Spec EnabledIf
    This commit adds the @EnabledIf annotation for specs. The way that Kotlin handles annotations isn't very fexible, and thus
    using this annotation is verbose and repetitive. We must reference a class, as we can't reference just a single method as we do for
    Test EnabledIf.
    
    This currently only works on JVM, as does @Ignored.
    
    Closes #1683

--- kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io.kotest.engine/extensions/IgnoredSpecDiscoveryExtension.kt
+++ kotest-framework/kotest-framework-engine/src/commonMain/kotlin/io.kotest.engine/extensions/IgnoredSpecDiscoveryExtension.kt
@@ -1,16 +1,24 @@
 package io.kotest.engine.extensions
 
+import io.kotest.core.annotation.EnabledIf
 import io.kotest.core.annotation.Ignored
 import io.kotest.core.extensions.DiscoveryExtension
 import io.kotest.core.spec.Spec
+import io.kotest.mpp.annotation
 import io.kotest.mpp.hasAnnotation
+import io.kotest.mpp.newInstanceNoArgConstructor
 import kotlin.reflect.KClass
 
 /**
- * Filters out any [Spec] annotated with @[Ignored].
+ * Filters out any [Spec] annotated with @[Ignored] or specs which [@EnabledIf] evaluates to false.
  */
 object IgnoredSpecDiscoveryExtension : DiscoveryExtension {
    override fun afterScan(classes: List<KClass<out Spec>>): List<KClass<out Spec>> {
-      return classes.filterNot { it.hasAnnotation<Ignored>() }
+      return classes
+         .filterNot { it.hasAnnotation<Ignored>() || !it.isEnabled() }
    }
+}
+
+private fun <T : Spec> KClass<T>.isEnabled(): Boolean {
+   return annotation<EnabledIf>()?.enabledIf?.newInstanceNoArgConstructor()?.enabled(this) ?: true
 }
